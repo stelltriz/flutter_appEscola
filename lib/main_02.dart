fe9274f8c41f7c74ba24b39107c8ee16c45d898f
stelltriz <stellbea@icloud.com>
2024-09-26 07:47:35 -0300

    até o main 5

--- lib/main_02.dart
+++ lib/main_02.dart
@@ -1,36 +1,26 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // classes que compoem os componentes da tela
+
 
 main(){
-  runApp(new Projeto01App());
+  runApp( Projeto01App());
 }
 
-class Projeto01App extends StatelessWidget{
-  Projeto01App({super.key});
+class Projeto01App extends  StatelessWidget{ // extendes pq e a classe pai
 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('App da Turma A', style: TextStyle(color: Colors.white),),
-          backgroundColor: Color.fromARGB(255, 231, 196, 255),
-        ),
-        body: Center(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Construindo App da turma A', style: TextStyle(fontSize: 24),),
-            SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed:() {}, 
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(200, 50),
-              ),
-              child: Text('Clique aqui'),
-              )
-          ],
-        ),
-      )
-    )
+ const Projeto01App ({super.key}); //constrtuor da claase , sempre vai estanciar aqui ou seja , sempre vai execuatr esse metodo primeiro
+  //seta como esses parametros em que se comportar e mapear os vaores
+
+//criando outro metodo , para construir a home
+//para criar a classe e carregar os componentes cria o widget
+  @override
+  Widget build(BuildContext context)  {
+    return  MaterialApp(
+      home: Scaffold(appBar:AppBar(
+        title: const Text("App da Turma A", style: TextStyle(color: Colors.white),),
+        backgroundColor: Color.fromARGB(255,64,60,134),
+      ) ,
+
+      ),
     );
-  }
+}
 }
--- lib/main_04.dart
+++ lib/main_04.dart
@@ -8,22 +8,24 @@ main(){
 
 class Projeto01App extends  StatelessWidget{ // extendes pq e a classe pai
 
- const  Projeto01App ({super.key}); //constrtuor da claase , sempre vai estanciar aqui ou seja , sempre vai execuatr esse metodo primeiro
+  const Projeto01App ({super.key}); //constrtuor da claase , sempre vai estanciar aqui ou seja , sempre vai execuatr esse metodo primeiro
   //seta como esses parametros em que se comportar e mapear os vaores
 
 //criando outro metodo , para construir a home
 //para criar a classe e carregar os componentes cria o widget
-@override
+  @override
   Widget build(BuildContext context)  {
     return  MaterialApp(
-      home: Scaffold(appBar:AppBar(
+      debugShowCheckedModeBanner: false, //Desativa a tarja de DEBUG
+      home: Scaffold(
+        appBar:AppBar(
         title: const Text("App da Turma A", style: TextStyle(color: Colors.white),),
         backgroundColor: Color.fromARGB(255,64,60,134),
       ) ,
       body: Center (
         child:Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        children:  [
           Text("Construindo App da Turma",style: TextStyle(fontSize: 24)
           ),
           SizedBox(height: 20),
@@ -37,8 +39,20 @@ class Projeto01App extends  StatelessWidget{ // extendes pq e a classe pai
         ],
       )
       ),
+
+    //acrescentar o rodape
+    bottomNavigationBar: BottomAppBar(
+      color: Color.fromARGB(255, 158, 155, 155),//Cor de fundo cinza claro
+      child:  Padding(
+      padding: const EdgeInsets.all(16.0),
+      child:Text(
+        'Aqui é o Rodapé',
+        textAlign: TextAlign.center,
+      ),
+
+      ),
+    ),
     ),
     );
-  
 }
 }
